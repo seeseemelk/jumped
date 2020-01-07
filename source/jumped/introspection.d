44459965a8cca930d196a73ba1bc4d1f4565c0b3
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2020-01-07 21:22:11 +0100

    Better introspection

--- source/jumped/introspection.d
+++ source/jumped/introspection.d
@@ -1,82 +1,30 @@
 module jumped.introspection;
 
 import std.traits;
-import std.traits : hasUDATrait = hasUDA;
 import std.algorithm.searching;
+import std.meta;
 
-/// Gets a list of all members of a type.
-string[] getMembers(T)() pure
+template HasRecursiveUDA(UDA, Symbol)
 {
-	static immutable members = [__traits(allMembers, T)];
-	static immutable objectMembers = [__traits(allMembers, Object)];
-	string[] typeMembers;
-	static foreach (member; members)
+	static if (hasUDA!(Symbol, UDA))
 	{
-		static if (member != "this" && !objectMembers.findAmong(member))
-		{
-			typeMembers ~= member;
-		}
+		alias HasRecursiveUDA = Alias!true;
 	}
-	return typeMembers;
+	else
+	{
+		alias HasRecursiveUDA = HasRecursiveUDA!(UDA, __traits(allMembers, Symbol));
+	}
 }
 
-@("getMembers returns a list of all members except members from Object")
+template HasRecursiveUDAFrom(UDA, Symbol, Members...)
+{
+	pragma(msg, Members);
+}
+
 unittest
 {
-	class TestClass
+	struct Hello
 	{
-		private int memberA;
-		void memberB() {}
+		
 	}
-
-	static immutable members = getMembers!TestClass;
-	static assert(members.length == 2);
-	static assert(members[0] == "memberA");
-	static assert(members[1] == "memberB");
-}
-
-/// Gets a list of all members that are functions of a type.
-string[] getFunctionMembers(T)() pure
-{
-	string[] functions;
-	static foreach (member; getMembers!T)
-	{
-		static if (isFunction!(__traits(getMember, T, member)))
-		{
-			functions ~= member;
-		}
-	}
-	return functions;
-}
-
-@("getFunctionMembers returns a list of all members that are functions")
-unittest
-{
-	class TestClass
-	{
-		int memberA;
-		void memberB() {}
-	}
-
-	static immutable members = getFunctionMembers!TestClass;
-	static assert(members.length == 1);
-	static assert(members[0] == "memberB");
-}
-
-/// Checks wether a type has a given UDA.
-template hasUDA(T, string member, UDA)
-{
-	alias hasUDA = hasUDATrait!(__traits(getMember, T, member), UDA);
-}
-
-@("hasUDA should return true if a member has the UDA")
-unittest
-{
-	struct uda1; // @suppress(dscanner.style.phobos_naming_convention)
-	class TestClass
-	{
-		@uda1 int a;
-	}
-	enum uda = hasUDA!(TestClass, "a", uda1);
-	static assert(uda == true);
 }
